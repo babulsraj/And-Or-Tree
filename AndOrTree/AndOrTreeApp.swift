535dcfa3819a1a452e3465df917acfe0ac5dd0ff
Babul Raj <babul.raj@moengagae.com>
2023-12-02 16:22:47 +0530

    Initial Commit

---
+++ AndOrTree/AndOrTreeApp.swift
@@ -0,0 +1,17 @@
+//
+//  AndOrTreeApp.swift
+//  AndOrTree
+//
+//  Created by Babul Raj on 02/12/23.
+//
+
+import SwiftUI
+
+@main
+struct AndOrTreeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
